035441bfc217c9ab306df500f34dc9a548b02919
Zihao Tan <3533438190@qq.com>
2026-03-19 20:45:30 +0800

    version3

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,9 +1,16 @@
 # Python program to add two numbers
 # Author: Tan Zihao
-# Using user input
+# Using function
+# Define a function to add two numbers
+def add(a, b):
+    # Convert to float and add
+    result = float(a) + float(b)
+    return result
+
 # Get user input
 a = input("First number: ")
 b = input("Second number: ")
-# Convert to float and add
-res = float(a) + float(b)
+# Call the function
+res = add(a, b)
+print("The result is: ")
 print(res)
